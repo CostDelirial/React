53156998625883320df5e88d5e948fd304eb4d70
NeuroCool <dnra.valverdetovar@protonmail.com>
2024-03-26 17:37:34 -0600

    gitignore

---
+++ contador/src/App.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+export function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className='App'>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count +1) }> + </button>
+    </div>
+    );
+}
